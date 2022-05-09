d57f5a8d4470fb29c4b1cc798dcdeaa8bc207815
xzawed <dirtchamber@naver.com>
2022-05-09 22:17:53 +0900

    COMMIT AND PUSH

--- Practics/Package/Errlog.py
+++ Practics/Package/Errlog.py
@@ -1,28 +1,36 @@
 ########################################################################################################################
-#  파일 생성이력
+#  파일 생성 이력
 #  일자 / 작업자 / 내용
-#  2022.04.11 / 이승호 / log파일 생성및 console화면에 출력처리
+#  2022.04.11 / 이승호 / log 파일 생성및 console 화면에 출력 처리
 ########################################################################################################################
 
-#  용도 : 텔레그램봇에서 발생한 에러내용을 콘솔화면에 출력 또는 파일저장을 하기위한 파일
-import logging # log기록
+#  용도 : 텔레 그램 봇 에서 발생한 에러 내용을 콘솔 화면에 출력 또는 파일 저장을 하기 위한 파일
+#  log 기록
+import logging
+#  traceback 프로 그램 에러
+import traceback
+
+import mysql
+
 
 ########################################################################################################################
-#  전역변수
+#  전역 변수
 #  log 설정
 Log = logging.getLogger('DEV_PYTHON_TELEGRAM_BOT_LOG')
 
 #  log Format
 #  LogLevel = logging.ERROR
 LogFileName = './Practics/log/DEV_PYTHON_TELEGRAM_BOT_Log.log'
-#  LogFormat = logging.Formatter('[%(process)d | %(thread)d | %(levelname)s | %(filename)s:%(lineno)s] %(asctime)s: %(message)s')
-LogFormat = logging.Formatter('[%(process)d | %(thread)d | %(filename)s:%(lineno)s] %(asctime)s: %(message)s')
+#  LogFormat = logging.Formatter('[%(process)d | %(thread)d | %(levelname)s | %(filename)s:%(lineno)s]
+#                                 %(asctime)s: %(message)s')
+LogFormat = logging.Formatter('TELEGRAM|[%(process)d | %(thread)d | %(filename)s:%(lineno)s] '
+                              '|::|%(levelname)s|::| %(asctime)s: %(message)s')
 
-#  Console = 콘솔화면에 출력
+#  Console = 콘솔 화면에 출력
 ConsoleHandler = logging.StreamHandler()
 ConsoleHandler.setFormatter(LogFormat)
 
-#  File = 파일에 저장처리
+#  File = 파일에 저장 처리
 FileHandler = logging.FileHandler(LogFileName)
 FileHandler.setFormatter(LogFormat)
 
@@ -32,8 +40,18 @@ Log.addHandler(ConsoleHandler)
 Log.addHandler(FileHandler)
 ########################################################################################################################
 
+
 ########################################################################################################################
 #  호출부 구현
-def SaveLog(err):
-    Log.error('MESSAGE : '+str(err))
+def savelog(state):
+    log = traceback.format_exc()
+
+    if state == "INFO":
+        Log.info('MESSAGE : '+str(log))
+    elif state == "ERROR":
+        Log.error('MESSAGE : '+str(log))
+
+    DB = mysql.MYSQL
+    DB.selmysql(self=DB ,opt='LOG', data=(state, str(log)))
+    print(state+" : "+str(log))
 ########################################################################################################################
--- main.py
+++ main.py
@@ -3,49 +3,58 @@
 #  Press Shift+F10 to execute it or replace it with your code.
 #  Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-#  텔레그램 bot 채팅방ID 확인
+#  텔레 그램 bot 채팅방 ID 확인
 #  https://api.telegram.org/bot5120813678:AAGz1vCMglGml4X5-eoTcFN3Y_JnWlFS6GY/getMe
 #  개인 : 1984552353
 #  그룹 : -697051008
 ########################################################################################################################
 
 ########################################################################################################################
-#  파일 생성이력
+#  파일 생성 이력
 #  일자 / 작업자 / 내용
-#  2022.04.11 / 이승호 / 텔레그램봇 명령어 입출력 처리및 로그, MariaDB SELECT 호출처리
-#  2022.04.16 / 이승호 / 번역기능 추가(추후 기능 심화예정)
+#  2022.04.11 / 이승호 / 텔레 그램 봇 명령어 입출력 처리및 로그, MariaDB SELECT 호출 처리
+#  2022.04.16 / 이승호 / 번역 기능 추가(추후 기능 심화 예정)
 ########################################################################################################################
 
 #  import os
-#  - os 모듈을 불러오는것
+#  - os 모듈을 불러 오는 것
 #  from os import *
-#  - os모듈로부터 모두(*)import
-#  언뜻보면 같은 의미인 것 같습니다만
-#  조금 다르답니다.어떻게 다른지 결론부터 말씀드리면 아래와 같습니다.
-#  --  한줄요약  --
-#  import만 사용하면 모듈 안의 함수를 사용할 때, 모듈명.함수명()으로 하고, from을 사용하면 바로 함수명()으로 사용
+#  - os 모듈 로부터 모두(*)import
+#  언뜻 보면 같은 의미인 것 같 습니다 만
+#  조금 다르 답니다.어떻게 다른지 결론 부터 말씀 드리면 아래와 같 습니다.
+#  --  한줄 요약  --
+#  import 만 사용 하면 모듈 안의 함수를 사용할 때, 모듈명.함수명()으로 하고, from 을 사용 하면 바로 함수명()으로 사용
 
 #  import telegram
 #  from telegram import *
 from telegram.ext import *
 from googletrans import *
 
-#  SERVER 환경설정을 위한내용
+#  SERVER 환경 설정을 위한 내용
 import platform
-#  내부IP 확인용
+#  내부 IP 확인용
 #  import socket
 import multiprocessing
 import requests
-#  traceback 프로그램 에러
-import traceback
 
-#  개별적으로 생성한내용
-import mysql
+#  SSH 접속
+#  cryptography ver: 36.0.2 down
+import paramiko
+
+import datetime
+
+#  개별적 으로 생성한 내용
+from mysql import *
+
 import Errlog
+
+
 ########################################################################################################################
-#  전역변수
-#  telegram token key와 chat room id 입력
-my_api_key = mysql.selmysql(opt='TOKEN')
+
+#  전역 변수
+#  telegram token key 와 chat room id 입력
+DB = MYSQL
+my_api_key = DB.selmysql(self=DB, opt='TOKEN', data=('TELEGRAM', '@xzawed_bot'))
 chat_room_id = -697051008
 my_server_env_os = platform.system()
 my_server_env_os_det = platform.platform()
@@ -53,146 +62,234 @@ my_server_env_os_ver = platform.version()
 my_server_env_cpu = platform.processor()
 my_server_env_cpu_cnt = multiprocessing.cpu_count()
 my_server_env_domain = "EXT IP ADDR : "+requests.get("https://api.ipify.org").text \
-                                  + " / domain : dirtchamber.iptime.org "
-
+                                  + " / domain : xzawed.iptime.org "
 #  telegram bot setting
-updater = Updater(token=my_api_key, use_context=True)  # bot에게 들어온 메시지가 있는지 체크
+updater = Updater(token=my_api_key, use_context=True)  # bot 에게 들어온 메시지 가 있는지 체크
 
 try:
     updater.dispatcher.stop()
     updater.job_queue.stop()
     updater.stop()
 except Exception:
-    err = traceback.format_exc()
-    Errlog.SaveLog(str(err))
+    Errlog.savelog('ERROR')
+
+
 ########################################################################################################################
+def execcommands():
+    #  SSH Client 접속 - 추후 해당 내용은 DB 에서 불러 오는 내용 으로 수정 예정
+    cli = paramiko.SSHClient()
+    cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    #  pwd = getpass.getpass("devuser11!")
+    cli.connect("xzawed.iptime.org", port=2153, username="devuser", password="devuser11!", look_for_keys=False, allow_agent=False)
+    #  명령어 실행
+    #  실행 결과를 표준 콘솔 입력(stdin), 표준 콘솔 출력(stdout), 표준 에러 출력(stderr)에 리턴 한다.
+    stdin, stdout, stderr = cli.exec_command("jupyter-notebook list")
+    #   실행한 명령어 에 대한 결과 텍스트
+    lines = stdout.readlines()
+    resultdata = ''.join(lines)
+    resultdata = resultdata[resultdata.find('?')+1:resultdata.find('::')]
+    print(resultdata)  # 결과 확인
+    cli.close()
+    return resultdata
+
 
 ########################################################################################################################
 #  응답부 구현
-#  명령어와 연결할 기능 구현
-#  아래부터는 특정 커맨드를 입력받으면 출력을 처리하는 단일커맨드 예제를 처리
-def helpprint(update, context):
+#  명령어 와 연결할 기능 구현
+#  아래 부터는 특정 커맨드 를 입력 받으면 출력을 처리 하는 단일 커맨드 예제를 처리
+def helpprinf(update, context):
     try:
-        context.bot.sendMessage(chat_id=chat_room_id, text=" *** 사용가능한 명령어 리스트 입니다 *** " + "\n\n" +
-                                                           " '/help' : 도움말 기능입니다. " + "\n\n" +
-                                                           " '/set' : hi,토큰,서버에 대한 정보를 제공합니다. " + "\n\n" +
-                                                           " '/google' : 구글에서 검색한 결과 url정보를 제공합니다. " + "\n\n" +
-                                                           " '/naver' : 네이버에서 검색한 결과 url정보를 제공합니다. " + "\n\n" +
-                                                           " '/tran' : 구글번역을 통한 한글을 영문으로 변환하여 출력합니다(추후 기능추가). " + "\n\n" +
-                                                           " 그외 여러 기능들이 추가로 개발될 예정입니다.")
+        context.bot.sendMessage(chat_id=chat_room_id, text=" *** 사용 가능한 명령어 리스트 입니다 *** " + "\n\n" +
+                                                           " '/help' : 도움말 기능 입니다. " + "\n\n" +
+                                                           " '/set' : hi,토큰,서버에 대한 정보를 제공 합니다. " + "\n\n" +
+                                                           " '/google' : 구글 에서 검색한 결과 url 정보 를 제공 합니다. " + "\n\n" +
+                                                           " '/naver' : 네이버 에서 검색한 결과 url 정보 를 제공 합니다. " + "\n\n" +
+                                                           " '/tran' : 구글 번역 을 통한 한글을 영문 으로 변환 하여 출력 합니다. " + "\n\n" +
+                                                           " 그외 여러 기능 들이 추가로 개발될 예정 입니다.")
     except Exception:
-        err = traceback.format_exc()
-        Errlog.SaveLog(str(err))
+        Errlog.savelog('ERROR')
 
-#  아래부터는 특정 키워드를 입력받으면 출력을 처리하는 단일커맨드 예제를 처리
+
+#  아래 부터는 특정 키워드 를 입력 받으면 출력을 처리 하는 단일 커맨드 예제를 처리
 #  (if 문을 이용한 처리 예제)
 def botsetprinf(update, context):
     try:
-        #  2022-04-09 : 사용자가 입력한 글자를 처리한다.
+        #  2022-04-09 : 사용자 가 입력한 글자를 처리 한다.
         keywords = ''
         for arg in context.args:
             keywords += '{}'.format(arg)+" "
 
-        #  맨마지막 글자 1자리를 제거한다(+)문자 제거
+        #  맨 마지막 글자 1자리를 제거 한다(+)문자 제거
         keywords = keywords[:-1]
 
         #  context.bot.sendMessage(chat_id=chat_room_id, text=keywords)
-        #  2022-04-09 : 상단에서 입력받은 값을 토대로 해당되는 결과값을 Return 한다.
+        #  2022-04-09 : 상단 에서 입력 받은 값을 토대로 해당 되는 결과 값을 Return 한다.
         if   keywords == "hi":
             context.bot.sendMessage(chat_id=chat_room_id, text="hello~")
         elif keywords == "서버":
-            context.bot.sendMessage(chat_id=chat_room_id, text=" ***텔레그램 BOT 개발서버 정보*** " + "\n"
+            context.bot.sendMessage(chat_id=chat_room_id, text=" ***텔레 그램 BOT 개발 서버 정보*** " + "\n"
                                                              + " OS : " + str(my_server_env_os) + "\n\n"
                                                              + "         " + str(my_server_env_os_det) + "\n\n"
                                                              + "         " + str(my_server_env_os_ver) + "\n\n"
                                                              + "         " + str(my_server_env_domain) + "\n\n"
                                                              + " CPU : " + str(my_server_env_cpu) + "\n\n"
                                                              + " CPU 갯수 : " + str(my_server_env_cpu_cnt))
+        elif keywords == "토큰":
+            jupyter_token = execcommands()
+            context.bot.sendMessage(chat_id=chat_room_id, text=" jupyter token : "+jupyter_token)
         else :
-            context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 은(는) 아직 설정되지 않은 단어입니다")
+            context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 은(는) 아직 설정 되지 않은 단어 입니다")
     except Exception:
-        err = traceback.format_exc()
-        Errlog.SaveLog(str(err))
+        Errlog.savelog('ERROR')
 
-#  아래부터는 웹사이트 검색기능 처리
+
+#  아래 부터는 웹 사이트 검색 기능 처리
 def botgoogleprinf(update, context):
     try:
-        #  입력한 검색어를 키워드변수에 조합한다.
+        #  입력한 검색어 를 키워드 변수에 조합 한다.
         keywords = ''
         for arg in context.args:
             keywords += '{}'.format(arg)+"+"
 
-        #  맨마지막 글자 1자리를 제거한다(+)문자 제거
+        #  맨 마지막 글자 1자리를 제거 한다(+)문자 제거
         keywords = keywords[:-1]
-
+        url = "https://www.google.com/search?q="
         context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 구글 검색")
-        context.bot.sendMessage(chat_id=chat_room_id, text="https://www.google.com/search?q="+keywords)
+        context.bot.sendMessage(chat_id=chat_room_id, text=url+keywords)
     except Exception:
-        err = traceback.format_exc()
-        Errlog.SaveLog(str(err))
+        Errlog.savelog('ERROR')
+
 
 def botnaverprinf(update, context):
     try:
-        #  입력한 검색어를 키워드변수에 조합한다.
+        #  입력한 검색어 를 키워드 변수에 조합 한다.
         keywords = ''
         for arg in context.args:
             keywords += '{}'.format(arg)+"+"
 
-        #  맨마지막 글자 1자리를 제거한다(+)문자 제거
+        #  맨 마지막 글자 1자리를 제거 한다(+)문자 제거
         keywords = keywords[:-1]
-
+        url = "https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="
         context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 네이버 검색")
-        context.bot.sendMessage(chat_id=chat_room_id, text="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query="+keywords)
+        context.bot.sendMessage(chat_id=chat_room_id, text=url+keywords)
     except Exception:
-        err = traceback.format_exc()
-        Errlog.SaveLog(str(err))
+        Errlog.savelog('ERROR')
 
-#  아래부터는 입력받은 메세지를 번역하는 기능을 제공
+
+#  아래 부터는 입력 받은 메세지 를 번역 하는 기능을 제공
 def botgoogletranprinf(update, context):
     try:
         keywords = ''
-        #  사용자가 입력한 단어를 문장으로 가공한다.
+        #  사용자 가 입력한 단어를 문장 으로 가공 한다.
         for arg in context.args:
             keywords += '{}'.format(arg)+" "
 
         translator = Translator()
 
-        #  번역대상 언어와 번역처리 언어를 추후 사용자가 지정할수 있게 처리 예정.
-        content = translator.translate(keywords,src='ko',dest='en')
+        #  번역 대상 언어와 번역 처리 언어를 추후 사용자 가 지정 할수 있게 처리 예정.
+        content = translator.translate(keywords, src='ko', dest='en')
 
-        #  번역한 문장을 변수처리
+        #  번역한 문장을 변수 처리
         sentence = content.text
 
-        context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 구글번역")
+        context.bot.sendMessage(chat_id=chat_room_id, text=keywords+" 구글 번역")
         context.bot.sendMessage(chat_id=chat_room_id, text=sentence)
     except Exception:
-        err = traceback.format_exc()
-        Errlog.SaveLog(str(err))
-
-#  아래부터는 주기적으로 특정메세지를 띄우는 예제를 처리
+        Errlog.savelog('ERROR')
 
 
+def botdateprinf(update, context):
+    try:
+        #  입력한 검색어 를 키워드 변수에 조합 한다.
+        keywords = ''
+        for arg in context.args:
+            keywords += '{}'.format(arg)+"+"
+
+        #  맨 마지막 글자 1자리를 제거 한다(+)문자 제거
+        keywords = keywords[:-1]
+
+        #  날짜 포멧과 요일 포멧
+        format_date = '%Y-%m-%d'
+        format_week = '%w'
+        str_date = str(keywords)
+
+        #  입력 받은 값의 길이가 8자리 일 경우
+        if len(str_date) == 8:
+            str_date = str_date[0:4]+"-"+str_date[4:6]+"-"+str_date[6:8]
+
+        #  1주
+        one_week = datetime.timedelta(weeks=1)
+
+        #  날짜형
+        #  현재 일자
+        try:
+            dt_date = datetime.datetime.strptime(str_date, format_date)
+        except Exception:
+            dt_date = datetime.datetime.now()
+
+        #  문자형
+        dy_str_date = dt_date.strftime(format_week)
+        #  숫자형
+        dy_int_date = int(dy_str_date)
+        dy_calc_date = dy_int_date
+        #  현재 일자 기준 현재 주차의 월요일 을 구한다.
+
+        i = 0
+        while dy_calc_date >= 1:
+            dy_calc_date = dy_int_date-i
+            if dy_calc_date == 1:
+                break
+            else:
+                i += 1
+
+        if dy_int_date == 0:
+            i = 6
+
+        #  입력한 날짜의 해당 주차의 월요일
+        mon_date = dt_date - datetime.timedelta(days=i)
+        #  다음주 일요일
+        nw_date = mon_date + one_week - datetime.timedelta(days=1)
+
+        #  문자형 으로 변경
+        dt_str_date = datetime.datetime.strftime(dt_date, format_date)
+        mon_str_date = datetime.datetime.strftime(mon_date, format_date)
+        nw_str_date = datetime.datetime.strftime(nw_date, format_date)
+
+        #  print(dt_date)
+        #  print(mon_date)
+        #  print(nw_date)
+        #  print(dy_str_date)
+        #  print(i)
+
+        context.bot.sendMessage(chat_id=chat_room_id, text="현재 일자 : "+dt_str_date + "\n"
+                                                           "금주 월요일 : "+mon_str_date + "\n"
+                                                           "금주 일요일 : "+nw_str_date)
+    except Exception:
+        Errlog.savelog('ERROR')
+
+
+#  아래 부터는 주기적 으로 특정 메세지 를 띄우는 예제를 처리
 ########################################################################################################################
 
 ########################################################################################################################
 #  호출부 구현
-#  기능과 명령어 연결("/hi" 명령어가 들어오면 TestPrint 함수가 실행됨)
-#  참고로 한글명령어가 안됨.
+#  기능과 명령어 연결("/hi" 명령어 가 들어 오면 TestPrint 함수가 실행됨)
+#  참고로 한글 명령어 가 안됨.
 try:
-    mysql.selmysql(opt='START')
-    #  command값을 upper로 일괄 변환해서 실행처리
-    updater.dispatcher.add_handler(CommandHandler("help".upper(), helpprint))
+    #  command 값을 upper 로 일괄 변환 해서 실행 처리
+    Errlog.savelog('INFO')
+    updater.dispatcher.add_handler(CommandHandler("help".upper(), helpprinf))
     updater.dispatcher.add_handler(CommandHandler('set'.upper(), botsetprinf, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('google'.upper(), botgoogleprinf, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('naver'.upper(), botnaverprinf, pass_args=True))
     updater.dispatcher.add_handler(CommandHandler('tran'.upper(), botgoogletranprinf, pass_args=True))
+    updater.dispatcher.add_handler(CommandHandler('date'.upper(), botdateprinf, pass_args=True))
+    DB.selmysql(self=DB, opt='TEMP', data='실행')
 
-    mysql.selmysql(opt='END')
-    err = traceback.format_exc()
-    Errlog.SaveLog(str(err))
+    #  err = traceback.format_exc()
+    #  Errlog.saveLog('INFO', str(err))
 except Exception:
-    err = traceback.format_exc()
-    Errlog.SaveLog(str(err))
+    Errlog.savelog('ERROR')
 ########################################################################################################################
 
 ########################################################################################################################
@@ -200,4 +297,3 @@ except Exception:
 updater.start_polling()
 updater.idle()
 ########################################################################################################################
-
